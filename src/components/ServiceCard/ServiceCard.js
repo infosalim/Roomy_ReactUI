3c96aeb1519238d39fde1d7d1044dd455ee9d9e5
Salim Hossain <salimh5757@gmail.com>
2020-12-14 00:59:00 +0600

    some changes

--- src/components/ServiceCard/ServiceCard.js
+++ src/components/ServiceCard/ServiceCard.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
-export default function({ thumbSrc, title, desc }){
-    return <div className='service-card'>
+export default function({ thumbSrc, title, desc, ...props }){
+    console.log(window.location.pathname);
+
+    if(window.location.pathname === '/'){
+        return <div className='service-card'>
+        <img src={thumbSrc} alt='thumbnail' />
+        <Link to='/extra-service'>{title}</Link>
+        <p>{desc}</p>
+    </div>
+    }
+    return <Link to='/extra-service' className='service-card'>
         <img src={thumbSrc} alt='thumbnail' />
         <h4>{title}</h4>
         <p>{desc}</p>
-    </div>
+    </Link>
 }
